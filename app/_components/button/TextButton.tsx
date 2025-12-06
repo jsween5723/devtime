32b4f219986b51a8bd0096ea908d1c7216ca8ec6
조현창 <gusckd57@naver.com>
2025-12-06 10:17:31 +0900

    style(button): text 버튼 추가

---
+++ app/_components/button/TextButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import styles from "./TextButton.module.css";
+
+type Props = {
+  children?: React.ReactNode;
+  onClickAction?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
+};
+
+export default function TextButton({
+  children,
+  onClickAction,
+  disabled,
+}: Props) {
+  return (
+    <button
+      className={styles.button}
+      onClick={onClickAction}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
