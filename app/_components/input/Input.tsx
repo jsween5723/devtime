98e69ac439e4cf059bb309f556f511b26bf086de
조현창 <gusckd57@naver.com>
2025-12-06 23:01:42 +0900

    style(input): Input 컴포넌트 추가

---
+++ app/_components/input/Input.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import styles from "./TextField.module.css";
+
+type Props = {
+  id?: string;
+  type?: "text" | "password" | "file" | "dropdown";
+  onChangeAction: React.ChangeEventHandler<HTMLInputElement>;
+  placeholder?: string;
+  value: any;
+  children?: React.ReactNode;
+};
+
+export default function Input({ children, ...others }: Props) {
+  return (
+    <section className={styles.section}>
+      <input
+        name={others.id}
+        type={others.type}
+        className={styles.input}
+        onChange={others.onChangeAction}
+        value={others.value}
+        placeholder={others.placeholder}
+      />
+      {children}
+    </section>
+  );
+}
